608a82593414393f0db81adaf5a5929aff303f7e
Danilenko Mike <mikexplorer@gmail.com>
2025-04-16 15:37:13 +0200

    feat: add config validation

--- errors.go
+++ errors.go
@@ -3,5 +3,7 @@ package telegramclient
 import "errors"
 
 var (
-	errEmptyToken = errors.New("token is empty")
+	errEmptyToken        = errors.New("token is empty")
+	errEmptyBotApiScheme = errors.New("bot api scheme is empty")
+	errEmptyBotApiHost   = errors.New("bot api host is empty")
 )
