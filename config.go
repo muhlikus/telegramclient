5af93a8c0469f4d3c3b480f74b3e6b68c800e82a
Danilenko Mike <mikexplorer@gmail.com>
2025-04-16 12:21:08 +0200

    added tags to config

--- config.go
+++ config.go
@@ -3,10 +3,10 @@ package telegramclient
 import "time"
 
 type Config struct {
-	Token                   string
-	botApiScheme            string
-	botApiHost              string
+	Token                   string `env:"TELEGRAM_BOT_TOKEN,notEmpty"`
+	BotApiScheme            string `env:"TELEGRAM_BOT_API_SCHEME" envDefault:"https"`
+	BotApiHost              string `env:"TELEGRAM_BOT_API_HOST" envDefault:"api.telegram.org"`
 	botApiPath              string
-	httpTimeout             time.Duration
-	httpTLSHandshakeTimeout time.Duration
+	HttpTimeout             time.Duration `env:"TELEGRAM_BOT_HTTP_TIMEOUT" envDefault:"2s"`
+	HttpTLSHandshakeTimeout time.Duration `env:"TELEGRAM_BOT_TLS_TIMEOUT" envDefault:"500ms"`
 }
